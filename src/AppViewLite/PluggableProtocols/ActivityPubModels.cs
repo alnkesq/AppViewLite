b6b291f4db01756f33dad83c718976088312d4d3
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-02-08 16:05:19 +0100

    Sort ActivityPub JSON fields

--- src/AppViewLite/PluggableProtocols/ActivityPubModels.cs
+++ src/AppViewLite/PluggableProtocols/ActivityPubModels.cs
@@ -6,21 +6,23 @@ using System.Threading.Tasks;
 
 namespace AppViewLite.PluggableProtocols.ActivityPub
 {
+    // Visual Studio -> Paste JSON as Classes
+    // Get-Clipboard | % { $_.Replace(' { get; set; }', ';') } | Sort-Object { $_.Substring($_.LastIndexOf(' ')) } | Select-Object -Unique | Set-Clipboard
 
     public class ActivityPubPostJson
     {
-        public string? in_reply_to_account_id;
-        public ActivityPubTag[] tags;
-        public string spoiler_text;
         public ActivityPubAccountJson account;
-        public string url;
-        public DateTime created_at;
-        public ActivityPubPostJson reblog;
-        public string content;
-        public ActivityPubMediaAttachmentJson[] media_attachments;
-        public string language;
-        public ActivityPubMentionJson[] mentions;
         public ActivityPubCard card;
+        public string content;
+        public DateTime created_at;
+        public string? in_reply_to_account_id;
+        public string language;
+        public ActivityPubMediaAttachmentJson[] media_attachments;
+        public ActivityPubMentionJson[] mentions;
+        public ActivityPubPostJson reblog;
+        public string spoiler_text;
+        public ActivityPubTag[] tags;
+        public string url;
     }
     public class ActivityPubMentionJson
     {
@@ -36,18 +38,18 @@ namespace AppViewLite.PluggableProtocols.ActivityPub
     }
     public class ActivityPubAccountJson
     {
-        public string fqn;
         public string acct;
-        public string display_name;
-        public long? statuses_count;
-        public long? following_count;
-        public long? followers_count;
-        public ActivityPubAccountField[] fields;
-        public string? note;
         public string? created_at;
-        public string? username;
-        public string? url;
+        public string display_name;
+        public ActivityPubAccountField[] fields;
+        public long? followers_count;
+        public long? following_count;
+        public string fqn;
         public ActivityPubNostr? nostr;
+        public string? note;
+        public long? statuses_count;
+        public string? url;
+        public string? username;
     }
     public class ActivityPubNostr
     {
@@ -66,25 +68,27 @@ namespace AppViewLite.PluggableProtocols.ActivityPub
 
     public class ActivityPubCard
     {
-        public string url;
-        public string title;
-        public string description;
-        public string language;
-        public string type;
         public string author_name;
         public string author_url;
+        public object[] authors;
+        public string blurhash;
+        public string description;
+        public string embed_url;
+        public int height;
+        public string html;
+        public string image_description;
+        public string image;
+        public string language;
         public string provider_name;
         public string provider_url;
-        public string html;
-        public int width;
-        public int height;
-        public string image;
-        public string image_description;
-        public string embed_url;
-        public string blurhash;
         public DateTime? published_at;
-        public object[] authors;
+        public string title;
+        public string type;
+        public string url;
+        public int width;
     }
+
+
 
 
 }

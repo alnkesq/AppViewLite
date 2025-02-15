2633afa364452deec92732efc4c88d1b7a90ae1a
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-02-15 14:38:34 +0100

    Avoid warning

--- src/AppViewLite/BlueskyEnrichedApis.cs
+++ src/AppViewLite/BlueskyEnrichedApis.cs
@@ -1233,7 +1233,7 @@ namespace AppViewLite
                 var message = at.AtError.Detail?.Message ?? at.AtError.Detail?.Error;
                 if (string.IsNullOrEmpty(message)) return subjectDisplayText + " returned error " + at.AtError.StatusCode;
 
-                if (message.StartsWith("Could not find repo:"))
+                if (message.StartsWith("Could not find repo:", StringComparison.Ordinal))
                     return "This user no longer exists at the specified PDS.";
 
                 return subjectDisplayText + " returned error " + message;

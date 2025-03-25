0fbd51d61840793bb079dc3bfcc7a32782a7498d
Alnkq <141324281+alnkesq@users.noreply.github.com>
2025-03-25 14:03:11 +0000

    No need to distinguish by IsDevelopment according to aspnet/core/host-and-deploy/proxy-load-balancer

--- src/AppViewLite.Web/Program.cs
+++ src/AppViewLite.Web/Program.cs
@@ -107,10 +107,10 @@ namespace AppViewLite.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                app.UseForwardedHeaders();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseForwardedHeaders();
 
             app.UseHttpsRedirection();
 

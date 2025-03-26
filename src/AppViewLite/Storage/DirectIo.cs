f43df9827050581b7c3474b88bae096e1ad2439b
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-03-26 18:18:04 +0100

    DirectIo

---
+++ src/AppViewLite/Storage/DirectIo.cs
@@ -0,0 +1,50 @@
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+
+namespace AppViewLite.Storage
+{
+    public static class DirectIo
+    {
+        public static SafeFileHandle Open(string path)
+        {
+            return MemoryMappedFileSlim.OpenFile(path, fileOptions: MemoryMappedFileSlim.NoBuffering | System.IO.FileOptions.RandomAccess);
+        }
+
+        public unsafe static NativeMemoryRange ReadAligned(SafeFileHandle handle, long fileOffset, int length, AlignedNativeArena arena)
+        {
+            var ptr = arena.Allocate((nuint)length);
+            var span = new Span<byte>(ptr, length);
+            var read = RandomAccess.Read(handle, span, fileOffset);
+            if (read != length)
+                throw new EndOfStreamException();
+            return new NativeMemoryRange((nuint)ptr, (nuint)length);
+        }
+
+        public unsafe static NativeMemoryRange ReadUnaligned(SafeFileHandle handle, long fileOffset, int length, AlignedNativeArena arena)
+        {
+            var blockSize = (int)arena.Alignment;
+            var offsetWithinBlock = fileOffset % blockSize;
+            var alignedFileOffset = fileOffset - offsetWithinBlock;
+            var end = fileOffset + length;
+
+
+            var offsetWithinEndBlock = end % blockSize;
+            long alignedFileOffsetEnd;
+            if (offsetWithinEndBlock == 0)
+                alignedFileOffsetEnd = end;
+            else
+                alignedFileOffsetEnd = end - offsetWithinEndBlock + blockSize;
+
+            var alignedLength = (int)(alignedFileOffsetEnd - alignedFileOffset);
+            var alignedSpan = ReadAligned(handle, alignedFileOffset, alignedLength, arena);
+            var unalignedSpan = new NativeMemoryRange(alignedSpan.Pointer + (nuint)offsetWithinBlock, (nuint)length);
+            return unalignedSpan;
+        }
+
+        public record struct NativeMemoryRange(nuint Pointer, nuint Length)
+        {
+        }
+    }
+}
+

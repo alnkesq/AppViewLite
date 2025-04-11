2e2417502f9eff94ef80a9a81f9d20a7b579e477
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-04-11 19:57:05 +0200

    Limit DuckDB resources

--- src/AppViewLite/Indexer.cs
+++ src/AppViewLite/Indexer.cs
@@ -1018,6 +1018,8 @@ namespace AppViewLite
                 checkGaps = true;
             }
             Log($"Incremental PLC directory bundle import since {prevDate?.ToString() ?? "beginning"}...");
+            mem.ExecuteNonQuery("SET memory_limit = '1GB';");
+            mem.ExecuteNonQuery("SET threads = 1;");
             var rows = mem.Execute<PlcDirectoryBundleParquetRow>($"from '{parquetFileOrDirectory}' where Date >= ?", prevDate ?? new DateTime(1980, 1, 1, 0, 0, 0, DateTimeKind.Utc))
                 .Select(row =>
                 {

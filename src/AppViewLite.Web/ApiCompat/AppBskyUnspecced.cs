ce32671372914c342ae7ad37db6ad67b1146d348
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-05-02 21:55:32 +0200

    Add Unspecced stubs

---
+++ src/AppViewLite.Web/ApiCompat/AppBskyUnspecced.cs
@@ -0,0 +1,30 @@
+using FishyFlip.Lexicon.App.Bsky.Unspecced;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppViewLite.Web.ApiCompat
+{
+    [Route("/xrpc")]
+    [ApiController]
+    [EnableCors("BskyClient")]
+    public class AppBskyUnspecced : Controller
+    {
+        [HttpGet("app.bsky.unspecced.getConfig")]
+        public IResult GetConfig()
+        {
+            return new GetConfigOutput
+            {
+            }.ToJsonResponse();
+        }
+        [HttpGet("app.bsky.unspecced.getTrendingTopics")]
+        public IResult GetTrendingTopics()
+        {
+            return new GetTrendingTopicsOutput
+            {
+                Suggested = [],
+                Topics = [],
+            }.ToJsonResponse();
+        }
+    }
+}
+

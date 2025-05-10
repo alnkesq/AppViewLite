839c151ae9f31cde95fa39e7338706c0b5fbd1bb
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-05-10 23:04:55 +0200

    XRPC: add empty unspecced stubs

--- src/AppViewLite.Web/ApiCompat/AppBskyUnspecced.cs
+++ src/AppViewLite.Web/ApiCompat/AppBskyUnspecced.cs
@@ -37,42 +37,67 @@ namespace AppViewLite.Web.ApiCompat
 
         public override Task<Results<ATResult<GetSuggestedFeedsOutput>, ATErrorResult>> GetSuggestedFeedsAsync([FromQuery] int? limit = 10, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetSuggestedFeedsOutput
+            {
+                Feeds = []
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetSuggestedFeedsSkeletonOutput>, ATErrorResult>> GetSuggestedFeedsSkeletonAsync([FromQuery] ATDid? viewer = null, [FromQuery] int? limit = 10, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetSuggestedFeedsSkeletonOutput
+            {
+                Feeds = []
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetSuggestedStarterPacksOutput>, ATErrorResult>> GetSuggestedStarterPacksAsync([FromQuery] int? limit = 10, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetSuggestedStarterPacksOutput
+            {
+                StarterPacks = []
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetSuggestedStarterPacksSkeletonOutput>, ATErrorResult>> GetSuggestedStarterPacksSkeletonAsync([FromQuery] ATDid? viewer = null, [FromQuery] int? limit = 10, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetSuggestedStarterPacksSkeletonOutput 
+            {
+                StarterPacks = []
+            }
+            .ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetSuggestedUsersOutput>, ATErrorResult>> GetSuggestedUsersAsync([FromQuery] string? category = null, [FromQuery] int? limit = 25, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetSuggestedUsersOutput
+            {
+                Actors = []
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetSuggestedUsersSkeletonOutput>, ATErrorResult>> GetSuggestedUsersSkeletonAsync([FromQuery] ATDid? viewer = null, [FromQuery] string? category = null, [FromQuery] int? limit = 25, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetSuggestedUsersSkeletonOutput
+            {
+                Dids = [],
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetSuggestionsSkeletonOutput>, ATErrorResult>> GetSuggestionsSkeletonAsync([FromQuery] ATDid? viewer = null, [FromQuery] int? limit = 50, [FromQuery] string? cursor = null, [FromQuery] ATDid? relativeToDid = null, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetSuggestionsSkeletonOutput
+            {
+                Actors = [],
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetTaggedSuggestionsOutput>, ATErrorResult>> GetTaggedSuggestionsAsync(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetTaggedSuggestionsOutput
+            {
+                Suggestions = [],
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetTrendingTopicsOutput>, ATErrorResult>> GetTrendingTopicsAsync([FromQuery] ATDid? viewer = null, [FromQuery] int? limit = 10, CancellationToken cancellationToken = default)
@@ -86,12 +111,18 @@ namespace AppViewLite.Web.ApiCompat
 
         public override Task<Results<ATResult<GetTrendsOutput>, ATErrorResult>> GetTrendsAsync([FromQuery] int? limit = 10, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetTrendsOutput
+            {
+                Trends = []
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<GetTrendsSkeletonOutput>, ATErrorResult>> GetTrendsSkeletonAsync([FromQuery] ATDid? viewer = null, [FromQuery] int? limit = 10, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new GetTrendsSkeletonOutput
+            {
+                Trends = [],
+            }.ToJsonResultOkTask();
         }
 
         public override Task<Results<ATResult<SearchActorsSkeletonOutput>, ATErrorResult>> SearchActorsSkeletonAsync([FromQuery] string q, [FromQuery] ATDid? viewer = null, [FromQuery] bool? typeahead = null, [FromQuery] int? limit = 25, [FromQuery] string? cursor = null, CancellationToken cancellationToken = default)
@@ -106,7 +137,10 @@ namespace AppViewLite.Web.ApiCompat
 
         public override Task<Results<ATResult<SearchStarterPacksSkeletonOutput>, ATErrorResult>> SearchStarterPacksSkeletonAsync([FromQuery] string q, [FromQuery] ATDid? viewer = null, [FromQuery] int? limit = 25, [FromQuery] string? cursor = null, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return new SearchStarterPacksSkeletonOutput
+            {
+                StarterPacks = []
+            }.ToJsonResultOkTask();
         }
     }
 }

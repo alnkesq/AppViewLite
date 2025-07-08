a3b19c5ae6d7c864b8d5bb690db72a22ebb5bdd8
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-07-08 20:28:11 +0200

    Don't count post-small-media-thumbnail as large images

--- src/AppViewLite.Web/wwwroot/app.js
+++ src/AppViewLite.Web/wwwroot/app.js
@@ -560,7 +560,7 @@ async function recordPostEngagement(postElement, kind) {
         kind += ',SeenInFollowingFeed';
     }
     console.log('Engagement: ' + kind + ' for /@' + postElement.dataset.postdid + '/' + postElement.dataset.postrkey);
-    var hasImages = !!postElement.querySelector('.post-image-list');
+    var hasImages = !!postElement.querySelector('.post-image-list') && !postElement.classList.contains('post-small-media-thumbnail');
     var weight = hasImages ? 2 : 1;
     safeSignalrInvoke('MarkAsRead', postElement.dataset.postdid, postElement.dataset.postrkey, kind, weight);
     postElement.wasMarkedAsRead = true;

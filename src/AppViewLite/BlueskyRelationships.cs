e793b34ad8203a267aa78fd27e9dcfa20e1f3544
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-10-18 18:28:45 +0200

    Increase follows probabilistic set size for 1B relationships

--- src/AppViewLite/BlueskyRelationships.cs
+++ src/AppViewLite/BlueskyRelationships.cs
@@ -288,7 +288,7 @@ namespace AppViewLite
 
             Likes = RegisterRelationshipDictionary<PostIdTimeFirst>("post-like-time-first", GetApproxTime24); //, new(1024 * 1024 * 1024, 6));
             Reposts = RegisterRelationshipDictionary<PostIdTimeFirst>("post-repost-time-first", GetApproxTime24); //, new(256 * 1024 * 1024, 10));
-            Follows = RegisterRelationshipDictionary<Plc>("follow", GetApproxPlc24, new(256 * 1024 * 1024, 6), _ => MultiDictionaryIoPreference.KeysAndOffsetsMmap, deletionProbabilisticCache: new KeyProbabilisticCache<Relationship, DateTime>(128 * 1024 * 1024, 4), zeroApproxTargetsAreValid: true);
+            Follows = RegisterRelationshipDictionary<Plc>("follow", GetApproxPlc24, new(512 * 1024 * 1024, 3), _ => MultiDictionaryIoPreference.KeysAndOffsetsMmap, deletionProbabilisticCache: new KeyProbabilisticCache<Relationship, DateTime>(128 * 1024 * 1024, 4), zeroApproxTargetsAreValid: true);
             Blocks = RegisterRelationshipDictionary<Plc>("block", GetApproxPlc24, new(64 * 1024 * 1024, 4), zeroApproxTargetsAreValid: true);
 
             Bookmarks = RegisterDictionary<Plc, BookmarkPostFirst>("bookmark");
